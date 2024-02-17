8b2d18a634c7b83f4073827376c82fe5fd3a7140
Kelvin Akaba <akabakelvin@gmail.com>
2024-02-17 12:18:58 +0000

    layout

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,12 +1,45 @@
 'use client'
 import React, { useState } from 'react'
 import { Button } from './ui/button'
+import { useToast } from "@/components/ui/use-toast"
+import { useForm } from 'react-hook-form'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage,} from "@/components/ui/form"
+import axios from 'axios'
+import { ToastAction } from '@radix-ui/react-toast'
 
 interface ContactFormProps{
     showHeader?: boolean,
 }
 
+interface FormData {
+    name: string;
+    email: string;
+    number: number,
+    subject: string,
+    message: string
+}
+
 const ContactForm: React.FC<ContactFormProps> = ({ showHeader = true }) => {
+    const { toast } = useToast()
+    const { register, handleSubmit, reset } = useForm<FormData>()
+
+    const onSubmit = async (data: FormData) => {
+        try {
+            const response = await axios.post('http://localhost:8000/api/messages/create', data)
+            console.log(response.data);
+            toast({ description: "Your message has been sent.",})
+            reset()
+        } catch (error) {
+            console.log(error);
+            toast({
+                variant: "destructive",
+                title: "Uh oh! Something went wrong.",
+                description: "There was a problem with your request.",
+                action: <ToastAction altText="Try again">Try again</ToastAction>,
+            })
+        }
+    }
+
   return (
     <section className="bg-white dark:bg-gray-900">
         <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
@@ -20,32 +53,68 @@ const ContactForm: React.FC<ContactFormProps> = ({ showHeader = true }) => {
                 </p>
             </div>
             )}
-            <form action="#" className="space-y-8">
+            
+                <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
                 <div>
                     <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Name</label>
-                    <input type="text" id="name" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Name" required />
+                    <input 
+                        type="text" 
+                        id="name"
+                        {...register('name', { required: true })} 
+                        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
+                        placeholder="Name" 
+                        required 
+                    />
                 </div>
                 <div>
                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-                    <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@gmail.com" required />
+                    <input
+                        type="email" 
+                        id="email"
+                        {...register('email', { required: true })}  
+                        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
+                        placeholder="name@gmail.com" 
+                        required 
+                    />
                 </div>
                 <div>
                     <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Number</label>
-                    <input type="tel" id="phone" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="735 783 8992" required />
+                    <input 
+                        type="tel" 
+                        id="phone"
+                        {...register('number')}  
+                        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
+                        placeholder="735 783 8992" 
+                        // required 
+                    />
                 </div>
                 <div>
                     <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-                    <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required />
+                    <input 
+                        type="text" 
+                        id="subject"
+                        {...register('subject')}  
+                        className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
+                        placeholder="Let us know how we can help you" 
+                        // required 
+                    />
                 </div>
                 <div className="sm:col-span-2">
                     <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-                    <textarea id="message" rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+                    <textarea
+                        required
+                        {...register('message', { required: true })}  
+                        id="message" 
+                        rows={6} 
+                        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" 
+                        placeholder="Leave a comment...">
+                    </textarea>
                 </div>
                 <div className='flex justify-center'>
-                    <Button>Send Message</Button>
+                    <Button type='submit'>Send Message</Button>
                 </div>
-                {/* <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button> */}
             </form>
+           
         </div>
     </section>
   )
